a4b1ddef9093a508c25f2359cedf3132c59e2ed5
Konstantin Bychkov <kbychkov@bcs.ru>
2018-11-16 06:00:03 +0300

    test: describe constructor

--- test/dailychart.test.js
+++ test/dailychart.test.js
@@ -1,22 +1,24 @@
 var expect = chai.expect;
 
 describe('dailychart.js', function () {
-  it('throws the error when no params passed', function () {
-    try {
-      new Dailychart();
-      expect(true).to.be.false; // should not get here
-    } catch (e) {
-      expect(e.message).to.be.equal('Dailychart.js: el is not defined');
-    }
-  });
+  describe('#constructor', function () {
+    it('throws the error when no params passed', function () {
+      try {
+        new Dailychart();
+        expect(true).to.be.false; // should not get here
+      } catch (e) {
+        expect(e.message).to.be.equal('Dailychart.js: el is not defined');
+      }
+    });
 
-  it('should initialize the empty chart without errors', function () {
-    try {
-      var element = document.createElement('div');
-      new Dailychart(element);
-    } catch (e) {
-      expect(e).to.be.false;
-    }
+    it('should initialize the empty chart without errors', function () {
+      try {
+        var element = document.createElement('div');
+        new Dailychart(element);
+      } catch (e) {
+        expect(e).to.be.false;
+      }
+    });
   });
 
   describe('#shift', function () {
